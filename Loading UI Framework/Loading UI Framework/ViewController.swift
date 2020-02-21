6416adb8b2152ccf9c95dbb9e775af5224f758e0
Enayatullah Naseri <enayatullahna@gmail.com>
2020-02-20 18:20:59 -0800

    Started the project

---
+++ Loading UI Framework/Loading UI Framework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loading UI Framework
+//
+//  Created by Enayatullah Naseri on 2/20/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
